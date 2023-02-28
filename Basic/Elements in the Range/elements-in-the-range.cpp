ed89bc75ae289ad718b9a30c061a16c9ef15cde9
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-02-28 20:50:03 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Elements in the Range/elements-in-the-range.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	
+	
+	public:
+	bool check_elements(int arr[], int n, int A, int B)
+	{
+		// Your code goes here
+		unordered_map<int,int>map;
+		
+		
+		for(int i=0;i<n;i++)
+		{
+		    map[arr[i]]++;
+		}
+		
+		for(int i=A;i<=B;i++)
+		{
+		    if(map[i]==0)
+		    {
+		        return false;
+		    }
+		}
+		
+		return true;
+	}
+
+
+		 
+
+};
+	
+
+//{ Driver Code Starts.
+
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+    	int n,A,B;
+		cin>>n>>A>>B;
+		int a[n];
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+
+        
+
+        Solution ob;
+        if (ob.check_elements(a, n, A, B))
+			cout << "Yes";
+		else
+			cout << "No";
+	    
+        
+	    cout << "\n";
+	     
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+// } Driver Code Ends
