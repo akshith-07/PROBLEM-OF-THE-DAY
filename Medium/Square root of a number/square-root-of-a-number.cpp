03cb77c6a5a58782863ab16f1c96b3a27f4cc7f7
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-03-02 21:55:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Square root of a number/square-root-of-a-number.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        // Your code goes here 
+        
+        int ans=0;
+        if(x==0)
+        {
+            return 0;
+        }
+        for(int i=1;i*i<=x;i++)
+        {
+            ans=i;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		Solution obj;
+		cout << obj.floorSqrt(n) << endl;
+	}
+    return 0;   
+}
+
+// } Driver Code Ends
