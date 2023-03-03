c616dd4e993f6427de6e4d6c9e16ce7e64462996
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-03-03 21:10:09 +0530

    Added Solution - GfG to GitHub

---
+++ School/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        //code here.
+        int l=s.length();
+        int c=count(s.begin(),s.end(),' ');
+        remove(s.begin(),s.end(),' ');
+        
+        s.resize(l-c);
+       
+       return s;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
