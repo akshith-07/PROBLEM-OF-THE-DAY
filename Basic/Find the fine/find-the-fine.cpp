d95c25ec1cfd43cb875d4946ff4db79673a8be92
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-03-15 22:18:32 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Find the fine/find-the-fine.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    long long int totalFine(int n, int date, int car[], int fine[])
+    {
+        
+        long long sum=0;
+        
+        int temp=date%2;
+        
+        if(temp==0)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(car[i]%2==1)
+                {
+                    sum+=fine[i];
+                }
+            }
+        }
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(car[i]%2==0)
+                {
+                    sum+=fine[i];
+                }
+            }
+            
+        }
+        
+        return sum;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i, date;
+        cin >> n >> date;
+        int car[n], fine[n];
+        
+        for (i = 0; i < n; i++){
+            cin >> car[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> fine[i];
+        }
+        Solution obj;
+        cout << obj.totalFine(n, date, car, fine) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
