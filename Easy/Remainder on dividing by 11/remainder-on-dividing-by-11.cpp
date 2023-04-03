7efbd7fa255ed202ab47582fcb088ed977dcbe5c
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-04-03 17:18:09 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remainder on dividing by 11/remainder-on-dividing-by-11.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution
+{
+
+public:
+    int xmod11(string x)
+    {
+       // code here
+        int num=0;
+        for(int i=0;i<x.length();i++)
+        {
+            num=(num*10)+int(x[i]);
+            num%=11;
+        }
+        return (num%11);    
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string x;
+        cin>>x;
+        Solution ob;
+        int ans = ob.xmod11(x);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
