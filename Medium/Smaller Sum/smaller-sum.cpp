012e87e7ebb16198f754ce305663507b4945cd1f
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-04-22 12:03:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smaller Sum/smaller-sum.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+public:
+    vector<long long> smallerSum(int n,vector<int> &arr){
+        // Code here
+        vector<long long>ans(n,0);
+        map<int,long long>mp;
+        for(auto i:arr)     mp[i]++;
+        long long prev=0;
+        for(auto &i:mp){
+            int freq=i.second;
+            i.second=prev;
+            prev=prev+(freq*i.first);
+        }
+        for(int i=0;i<n;i++){
+            ans[i]=mp[arr[i]];
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob; 
+        vector<long long> ans=ob.smallerSum(n,arr);
+        for(int i=0;i<n;i++){
+            if(i!=n-1){
+                cout<<ans[i]<<" ";
+            }
+            else{
+                cout<<ans[i]<<endl;
+            }
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
