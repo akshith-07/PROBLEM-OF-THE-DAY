84a7c139b4a21170a35067b90b03ce3f1b2787cd
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-06-19 18:34:12 +0530

    Updated Solution - GfG to GitHub

--- Medium/Rearrange an array with O(1) extra space/rearrange-an-array-with-o1-extra-space.cpp
+++ Medium/Rearrange an array with O(1) extra space/rearrange-an-array-with-o1-extra-space.cpp
@@ -5,27 +5,21 @@ using namespace std;
 
 // } Driver Code Ends
     
-
 class Solution{
     public:
-    // arr: input array
-    // n: size of array
-    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
-    //with O(1) extra space.
-    void arrange(long long arr[], int n) {
-        // Your code here
-        
-       
-        for(int i=0; i<n; i++)
-        {
-            int index = arr[i]%n;
-            arr[i]+= (arr[index]%n)*n;
-        }
-        
-        for(int i=0; i<n; i++){
-            arr[i] = arr[i]/n;
-        }
-        
+        void arrange(long long arr[], int n){
+            int maxB=log2(100000)+1;
+            for(int i=0;i<n;i++){
+                int t = arr[arr[i]];
+                for(int j=0;j<maxB;j++){
+                    if(((t>>j)&1)==1){
+                        arr[i] = arr[i] + (1LL<<(maxB+j+1));
+                    }
+                }
+            }
+            for(int i=0;i<n;i++){
+                arr[i]=(arr[i]>>(maxB+1));
+            }
     }
 };
 
