ea7f76f2d4db32df5992d1214b4beac2bb2ef79b
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-06-23 16:33:12 +0530

    Updated Solution - GfG to GitHub

--- Medium/Majority Element/majority-element.cpp
+++ Medium/Majority Element/majority-element.cpp
@@ -13,26 +13,72 @@ class Solution{
      // Function to find majority element in the array
     // a: input array
     // size: size of input array
-    int majorityElement(int arr[], int size)
+    int majorityElement(int v[], int size)
     {
+    
+    // METHOD 1-HASH MAP
         
-       unordered_map<int , int>umap;
+    //   unordered_map<int , int>umap;
        
-       for(int i=0;i<size;i++)
-       {
-           umap[arr[i]]++;
+    //   for(int i=0;i<size;i++)
+    //   {
+    //       umap[arr[i]]++;
           
-       }
+    //   }
        
-       for(auto it:umap)
-       {
-           if(it.second>size/2)
-           {
-               return it.first;
-           }
-       }
+    //   for(auto it:umap)
+    //   {
+    //       if(it.second>size/2)
+    //       {
+    //           return it.first;
+    //       }
+    //   }
        
-       return -1;
+    //   return -1;
+    
+    //METHOD-2--->MOORE'S VOTING ALGO
+    
+    int count=0;
+    
+    int ele;
+    
+    
+    for(int i=0;i<size;i++)
+    {
+        if(count==0)
+        {
+            ele=v[i];
+            count=1;
+        }
+        else if(v[i]==ele)
+        {
+            count++;
+            
+        }
+        else
+        {
+            count--;
+        }
+    }
+    
+    int count1=0;
+    for(int i=0;i<size;i++)
+    {
+        if(ele==v[i])
+        {
+            count1++;
+        }
+    }
+    
+    if(count1>size/2)
+    {
+        return ele;
+    }
+    
+    return -1;
+    
+    
+    
        
     }
 };
