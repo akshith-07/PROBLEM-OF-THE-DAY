2c4ca38499bcbaaed966e9ed9528c12f2c93b6a4
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-06-24 15:10:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pattern 22/pattern-22.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+        
+        for(int i=0;i<2*n-1;i++)
+        {
+            for(int j=0;j<2*n-1;j++)
+            {
+                int top=i;
+                int left=j;
+                
+                int bottom=(2*n-2)-i;
+                int right=(2*n-2)-j;
+                
+                cout<<(n-min(min(top,left),min(bottom,right)))<<" ";
+            }
+            cout<<endl;
+            
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
