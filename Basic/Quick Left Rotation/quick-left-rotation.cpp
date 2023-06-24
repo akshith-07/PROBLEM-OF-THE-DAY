964f106a7f78892ff15c9d39dae7a78078f6d75b
AKSHITH JOBIRIN S <94063554+akshith-07@users.noreply.github.com>
2023-06-24 10:34:34 +0530

    Updated Solution - GfG to GitHub

--- Basic/Quick Left Rotation/quick-left-rotation.cpp
+++ Basic/Quick Left Rotation/quick-left-rotation.cpp
@@ -9,25 +9,32 @@ class Solution{
 	public:
 	void leftRotate(int arr[], int k, int n) 
 	{ 
-	   // Your code goes here
+	    //BRUTE APPROACH
 	   
-	   k=k%n;
-	   int temp[k];
-	   for(int i=0;i<k;i++)
-	   {
-	       temp[i]=arr[i];
+	   //k=k%n;
+	   //int temp[k];
+	   //for(int i=0;i<k;i++)
+	   //{
+	   //    temp[i]=arr[i];
 	       
-	   }
+	   //}
 	   
-	   for(int i=k;i<n;i++)
-	   {
-	       arr[i-k]=arr[i];
-	   }
+	   //for(int i=k;i<n;i++)
+	   //{
+	   //    arr[i-k]=arr[i];
+	   //}
 	   
-	   for(int i=n-k;i<n;i++)
-	   {
-	       arr[i]=temp[i-(n-k)];
-	   }
+	   //for(int i=n-k;i<n;i++)
+	   //{
+	   //    arr[i]=temp[i-(n-k)];
+	   //}
+	   
+	   
+	   //OPTIMAL APPROACH
+	   k=k%n;
+	   reverse(arr,arr+k);
+	   reverse(arr+k,arr+n);
+	   reverse(arr,arr+n);
 	} 
 		 
 
